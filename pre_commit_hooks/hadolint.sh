31109d5cc369e7affa8e37aea89498cf35b6e66a
sum41k <42783769+sum41k@users.noreply.github.com>
2020-04-06 18:50:07 +0300

    Update hadolint.sh

--- pre_commit_hooks/hadolint.sh
+++ pre_commit_hooks/hadolint.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if which hadolint &> /dev/null $? != 0 ; then
-    echo "Hadolint must be installed"
+set -o errexit
+set -o pipefail
+set -o nounset
+
+export PATH=$PATH:/usr/local/bin
+
+if ! which hadolint &>/dev/null; then
+    >&2 echo "Hadolint must be installed" 
     exit 1
 fi
 
 hadolint "$@"
-exit $?
